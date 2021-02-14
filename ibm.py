fe731b2d288f36b355e92ff98bd5dc3cd2a08fc6
karstenWERNER <seremaster@gmail.com>
2021-02-14 01:35:17 +0100

    cleanup3

--- ibm.py
+++
@@ -1,8 +0,0 @@
-import string
-from lazypredict.Supervised import LazyClassifier, LazyRegressor
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
-a = list(string.ascii_uppercase)
-
-for company in 'HAL':
-     print(a[a.index(company) + 1])
